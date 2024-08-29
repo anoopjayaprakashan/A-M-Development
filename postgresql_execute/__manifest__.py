6746e2d8dd54eebc38d7f4f79fbe9d743c499549
anoop <anoopjayaprakash@gmail.com>
2024-08-29 11:14:34 +0400

    update python_execute

--- postgresql_execute/__manifest__.py
+++ postgresql_execute/__manifest__.py
@@ -12,7 +12,7 @@
     "summary": """ Module to execute postgresql query """,
     "category": "Extra Tools",
     "version": "17.0.1.0.0",
-    "author": "A & M",
+    "author": "Anoop",
     "license": "LGPL-3",
     "depends": ['base', 'mail'],
     "data": ['security/ir.model.access.csv',
