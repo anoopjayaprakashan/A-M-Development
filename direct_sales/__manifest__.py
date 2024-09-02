dce22b243e45f50f56e6b7aecd0d7717bea81d10
anoop <anoopjayaprakash@gmail.com>
2024-09-02 13:58:20 +0400

    [Add New Module DIrect Sale]

---
+++ direct_sales/__manifest__.py
@@ -0,0 +1,23 @@
+# -*- coding: utf-8 -*-
+###################################################################################
+
+# Author       :  Anoop
+# Copyright(c) :  2024-Present.
+# License      :  LGPL-3.0 or later (http://www.gnu.org/licenses/lgpl.html).
+
+###################################################################################
+
+{
+    "name": "Direct Sales",
+    "summary": """Module for Complete Sales Operations in a Single Function""",
+    "category": "Extra Tools",
+    "version": "17.0.1.0.0",
+    "author": "Anoop",
+    "license": "LGPL-3",
+    "depends": ['base', 'mail', 'sale_management', 'stock'],
+    "data": [],
+    "images": [],
+    "installable": True,
+    "application": False,
+    "auto_install": False,
+}
